c0719738d37e75e8734eca079f087b98a37b0663
Kevin <kevpadilla01@gmail.com>
2024-10-28 14:18:18 -0300

    connection to mongodb added

---
+++ db.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import { config as configDotenv } from "dotenv";
+
+// Cargar variables de entorno
+configDotenv();
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Connected to MongoDB');
+  } catch (err) {
+    console.error('Error connecting to MongoDB:', err);
+    process.exit(1)
+  }
+};
+
+export default connectDB;
