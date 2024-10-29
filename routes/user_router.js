2c15e7cfa4da22189bb1f74cfb5d2257033a22e3
Kevin <kevpadilla01@gmail.com>
2024-10-28 21:33:56 -0300

    ft: user router added

---
+++ routes/user_router.js
@@ -0,0 +1,15 @@
+import { Router } from 'express'
+import {
+  register,
+  login,
+  verifyToken,
+  logout
+} from '../controllers/user_controller.js'
+import { registerSchema, loginSchema } from '../schemas/userSchema.js'
+import { validateSchema } from '../middlewares/validator_middleware.js'
+export const authRouter = Router()
+
+authRouter.post('/register', validateSchema(registerSchema), register) // register req
+authRouter.post('/login', validateSchema(loginSchema), login) // login req
+authRouter.get('/verify', verifyToken) // verify token req
+authRouter.post('/logout', logout) // logout req
