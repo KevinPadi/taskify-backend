004d1103599749ddc037587af93cc94a85759c6f
Kevin <kevpadilla01@gmail.com>
2024-10-28 21:55:28 -0300

    fix: spelling error in the schema name in boardRouter

--- routes/board_router.js
+++ routes/board_router.js
@@ -7,11 +7,11 @@ import {
 } from '../controllers/board_controller.js'
 import { auth } from '../middlewares/auth_middleware.js'
 import { validateSchema } from '../middlewares/validator_middleware.js'
-import { createBoardSchema } from '../schemas/boards_schema.js'
+import { boardSchema } from '../schemas/boards_schema.js'
 
 export const boardsRouter = Router()
 
 boardsRouter.get('/boards', auth, getBoards)
-boardsRouter.post('/boards', auth, validateSchema(createBoardSchema), createBoard)
+boardsRouter.post('/boards', auth, validateSchema(boardSchema), createBoard)
 boardsRouter.patch('/boards/:id', auth, updateBoard)
 boardsRouter.delete('/boards/:id', auth, deleteBoard)
