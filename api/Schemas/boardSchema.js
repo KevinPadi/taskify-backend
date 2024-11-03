662c497cd14ee3208b9c26f9ffc54f8cb1c8c433
Kevin <kevpadilla01@gmail.com>
2024-11-03 15:56:55 -0300

    trying to deploy on render.com

--- api/Schemas/boardSchema.js
+++ api/Schemas/boardSchema.js
@@ -1,7 +1,9 @@
 import { z } from 'zod'
 
-export const boardSchema = z.object({
+const boardSchema = z.object({
   name: z.string({
     required_error: 'Name is required'
   })
 })
+
+export default boardSchema
