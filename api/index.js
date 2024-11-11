43f1b6e4abfdc7af0be070ff8f6312fd0751869a
Kevin Padilla <kevpadilla01@gmail.com>
2024-11-10 21:43:31 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,18 +1,20 @@
-import express, { json } from "express"
-import { config as configDotenv } from "dotenv"
-import connectDB from "./config/db.js"
-import cors from "cors"
-import cookieParser from "cookie-parser"
-import apiRouter from "./routes/apiRouter.js"
+import express, { json } from "express";
+import { config as configDotenv } from "dotenv";
+import connectDB from "./config/db.js";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import apiRouter from "./routes/apiRouter.js";
 
-configDotenv()
+configDotenv();
 
-const app = express()
+const app = express();
 
-app.disable('X-powered-by')
-connectDB()
+app.disable('X-powered-by');
+connectDB();
 
-app.use(json())
+app.use(json());
+
+// Configuración de CORS con el header adicional
 app.use(
   cors({
     origin: 'http://localhost:5173',
@@ -20,14 +22,20 @@ app.use(
   })
 );
 
-app.use(cookieParser())
+// Middleware para agregar el header Access-Control-Allow-Credentials
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
+app.use(cookieParser());
 
 app.use('/api', apiRouter);
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
-  console.log(`Server listening on http://localhost:${PORT}`)
-})
+  console.log(`Server listening on http://localhost:${PORT}`);
+});
 
-export default app
+export default app;
